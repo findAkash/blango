a5b3d1db709e738acee06dfd9d8a5bf17e9d8617
Akash Karmacharya <karmachrya@uni.coventry.ac.uk>
2022-11-07 13:36:23 +0000

    Finish custom template tags

--- blango/urls.py
+++ blango/urls.py
@@ -20,4 +20,5 @@ import blog.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index),
+    path("post/<slug>/", blog.views.post_details, name="blog-post-detail"),
 ]
--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@
 
 # urlpatterns = [
 #   path('', views.index),
+
 # ]
